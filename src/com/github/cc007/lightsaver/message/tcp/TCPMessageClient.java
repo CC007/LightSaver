b4d7ab1d87ba56fa58ad739bffa1997751256bc2
CC007 <Coolcat_the_best2@hotmail.com>
2015-03-01 00:57:19 +0100

    removed unused imports

--- src/com/github/cc007/lightsaver/message/tcp/TCPMessageClient.java
+++ src/com/github/cc007/lightsaver/message/tcp/TCPMessageClient.java
@@ -10,7 +10,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
--- src/com/github/cc007/lightsaver/message/udp/UDPMessageClient.java
+++ src/com/github/cc007/lightsaver/message/udp/UDPMessageClient.java
@@ -12,7 +12,6 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
