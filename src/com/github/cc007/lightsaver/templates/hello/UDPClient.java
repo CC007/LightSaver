2dc7b3a09ebf169b5f9c8d906ea1ce0926da52db
CC007 <Coolcat_the_best2@hotmail.com>
2015-02-28 23:48:08 +0100

    indentation and other formatting

--- src/com/github/cc007/lightsaver/templates/hello/UDPClient.java
+++ src/com/github/cc007/lightsaver/templates/hello/UDPClient.java
@@ -8,28 +8,29 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class UDPClient {
-	private static String SERVER_ADDRESS = "localhost";
 
-	public static void main(String[] args) {
-		DatagramSocket aSocket = null;
-		try {
-			aSocket = new DatagramSocket();
-			byte[] m = "Hello world".getBytes();
-			InetAddress aHost = InetAddress.getByName(SERVER_ADDRESS);
-			DatagramPacket request = new DatagramPacket(m, m.length, aHost,
-					UDPServer.SERVER_PORT);
-			aSocket.send(request);
-			// get reply
-			byte[] buffer = new byte[1000];
-			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-			aSocket.receive(reply);
-			System.out.println("Reply: " + new String(reply.getData()));
-		} catch (SocketException e) {
-			e.printStackTrace();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private static String SERVER_ADDRESS = "localhost";
+
+    public static void main(String[] args) {
+        DatagramSocket aSocket = null;
+        try {
+            aSocket = new DatagramSocket();
+            byte[] m = "Hello world".getBytes();
+            InetAddress aHost = InetAddress.getByName(SERVER_ADDRESS);
+            DatagramPacket request = new DatagramPacket(m, m.length, aHost,
+                    UDPServer.SERVER_PORT);
+            aSocket.send(request);
+            // get reply
+            byte[] buffer = new byte[1000];
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+            aSocket.receive(reply);
+            System.out.println("Reply: " + new String(reply.getData()));
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
