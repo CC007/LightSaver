7908c6007feb33fe1b8234b3fcade7b18728b200
CC007 <Coolcat_the_best2@hotmail.com>
2015-03-01 03:05:46 +0100

    comments

--- src/com/github/cc007/lightsaver/message/MessageTypes.java
+++ src/com/github/cc007/lightsaver/message/MessageTypes.java
@@ -16,15 +16,13 @@ public interface MessageTypes {
     // declare the max size of all message types below
     public static final int MAX_UDP_MSG_SIZE = 12;
 
-    // put here the type representation
+    /* put here the type representation */
     // udp messages
     // - detector messages
     public static final int LIGHT_DETECTOR_MSG = 1000;      //size: 12
     public static final int PASSAGE_DETECTOR_MSG = 1010;    //size:  8
     public static final int MOTION_DETECTOR_MSG = 1020;     //size:  8
 
-    // - light state change messages
-    //     none
     // tcpmessages
     // - detector messages
     public static final int DOOR_DETECTOR_MSG = 2000;
