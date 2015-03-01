5cd4a8aa0c74dff91088f4eb344dc40c2689d8bf
CC007 <Coolcat_the_best2@hotmail.com>
2015-03-01 01:57:37 +0100

    added doordetectorclient and corresponding classes

---
+++ src/com/github/cc007/lightsaver/detector/door/DoorDetectorClient.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.cc007.lightsaver.detector.door;
+
+import com.github.cc007.lightsaver.message.Message;
+import com.github.cc007.lightsaver.message.MessageTypes;
+import com.github.cc007.lightsaver.message.tcp.TCPMessageClient;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+/**
+ *
+ * @author Rik
+ */
+public class DoorDetectorClient extends TCPMessageClient {
+
+    protected int clientId;
+    private boolean doorstate;
+
+    public DoorDetectorClient(int clientId) {
+        super("Door detector #" + clientId);
+        this.clientId = clientId;
+    }
+
+    private boolean getDoorState() {
+        Random r = new Random(System.currentTimeMillis());
+        return r.nextBoolean();
+    }
+
+    @Override
+    protected void doBefore() {
+        send = getDoorState() == doorstate;
+        doorstate = send ? !doorstate : doorstate;
+    }
+
+    @Override
+    protected Message createMessage() {
+        return new DoorDetectorMessage(MessageTypes.DOOR_DETECTOR_MSG, clientId, doorstate);
+    }
+
+    @Override
+    protected byte[] writeToBuffer() {
+        return ByteBuffer
+                .allocate(9)
+                .putInt(0, m.getMsgType())
+                .putInt(4, ((DoorDetectorMessage) m).getClientId())
+                .put(8, ((DoorDetectorMessage) m).isOpen() ? (byte) 1 : (byte) 0)
+                .array();
+    }
+
+}
